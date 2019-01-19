b90cdecfb73273ffc823e48c3f24e4b8fdd79e54
Duncan Leo <duncanleo97@gmail.com>
2019-01-20 01:52:57 +0800

    disconnect in /suicide command

--- commands/suicide.go
+++ commands/suicide.go
@@ -8,5 +8,8 @@ import (
 
 func suicide(s *discordgo.Session, m *discordgo.MessageCreate) {
 	s.ChannelMessageSend(m.ChannelID, "Goodbye, cruel world!")
+	if VoiceConnection != nil {
+		VoiceConnection.Disconnect()
+	}
 	os.Exit(1)
 }

e2357dfd4de68d9cac035a250a1d62bc40234474
Duncan Leo <hello@duncanleo.me>
2019-10-25 22:49:42 +0800

    sanitise banned words for regex

--- util/tts.go
+++ util/tts.go
@@ -12,17 +12,26 @@ var (
 		"music video",
 		"special video",
 		"lyric video",
+		"official video",
 		"M/?V",
 		"colou?r coded lyrics",
 		"1080p",
 		"720p",
+		"(Audio)",
 	}
-	ttsBannedWordsRegex = regexp.MustCompile(fmt.Sprintf("(?i)(%s)", strings.Join(ttsBannedWords, "|")))
 )
+
+func ttsBannedWordsRegex() *regexp.Regexp {
+	var sanitisedWords []string
+	for _, word := range ttsBannedWords {
+		sanitisedWords = append(sanitisedWords, regexp.QuoteMeta(word))
+	}
+	return regexp.MustCompile(fmt.Sprintf("(?i)(%s)", strings.Join(sanitisedWords, "|")))
+}
 
 // SanitiseSongTitleTTS process a song title for TTS reading
 func SanitiseSongTitleTTS(title string) string {
-	return ttsBannedWordsRegex.
+	return ttsBannedWordsRegex().
 		ReplaceAllString(
 			title,
 			"",

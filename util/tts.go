ac090bfa971ad2a32682ccf9ed4483b5ed9103a2
Duncan Leo <hello@duncanleo.me>
2020-05-06 23:15:01 +0800

    feat: improve video title sanitisation

--- util/tts.go
+++ util/tts.go
@@ -1,39 +1,50 @@
 package util
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 )
 
 var (
-	ttsBannedWords = []string{
-		"official",
-		"music video",
-		"special video",
-		"lyric video",
-		"official video",
-		"M/?V",
-		"colou?r coded lyrics",
-		"1080p",
-		"720p",
-		"(Audio)",
+	// Special cases
+	regExes = []*regexp.Regexp{
+		regexp.MustCompile(`(?i)Han.+?Rom.+?Eng`),
+		regexp.MustCompile(`(?i)music video`),
+		regexp.MustCompile(`(?i)special video`),
+		regexp.MustCompile(`(?i)lyric video`),
+		regexp.MustCompile(`(?i)official video`),
+		regexp.MustCompile(`(?i)official audio`),
+		regexp.MustCompile(`(?i)color coded lyrics`),
+		regexp.MustCompile(`(?i)colour coded lyrics`),
+		regexp.MustCompile(`(?i)1080p`),
+		regexp.MustCompile(`(?i)720p`),
+		regexp.MustCompile(`(?i)official`),
+		regexp.MustCompile(`(?i)(Audio)`),
+		regexp.MustCompile(`(?i)(Lyrics)`),
+		regexp.MustCompile(`(?i)(Lyric)`),
+	}
+
+	// Case sensitive replacement
+	bannedTerms = []string{
+		"官方",
+		"完整版",
+		"MV",
+		"M/V",
+
+		// Remove empty brackets
+		"()",
+		"[]",
 	}
 )
 
-func ttsBannedWordsRegex() *regexp.Regexp {
-	var sanitisedWords []string
-	for _, word := range ttsBannedWords {
-		sanitisedWords = append(sanitisedWords, regexp.QuoteMeta(word))
-	}
-	return regexp.MustCompile(fmt.Sprintf("(?i)(%s)", strings.Join(sanitisedWords, "|")))
-}
-
 // SanitiseSongTitleTTS process a song title for TTS reading
 func SanitiseSongTitleTTS(title string) string {
-	return ttsBannedWordsRegex().
-		ReplaceAllString(
-			title,
-			"",
-		)
+	var result = title
+	for _, rgx := range regExes {
+		result = rgx.ReplaceAllString(result, "")
+	}
+	for _, bannedTerm := range bannedTerms {
+		result = strings.ReplaceAll(result, bannedTerm, "")
+	}
+	return result
 }

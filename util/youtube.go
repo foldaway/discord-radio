98c04df61e625bd5a6ba0215f0a53e27cf75381a
Duncan Leo <hello@duncanleo.me>
2021-12-22 12:57:22 +0800

    refactor: migrate playlist retrieval to youtube-dl

--- util/youtube.go
+++ util/youtube.go
@@ -1,30 +1,27 @@
 package util
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"github.com/andersfylling/disgord/json"
 	"log"
 	"math/rand"
-	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"sort"
 	"strings"
 	"time"
 
 	"github.com/go-co-op/gocron"
-	"google.golang.org/api/googleapi/transport"
-
-	"google.golang.org/api/youtube/v3"
-
 	"github.com/joho/godotenv"
 
 	cryptoRand "crypto/rand"
 )
 
 var (
-	youtubeService        *youtube.Service
-	autoPlaylistItemCache = make([]*youtube.PlaylistItem, 0)
+	autoPlaylistItemCache = make([]PlaylistItem, 0)
 )
 
 func init() {
@@ -41,17 +38,11 @@ func init() {
 		rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
 	}
 
-	client := &http.Client{
-		Transport: &transport.APIKey{Key: os.Getenv("GOOGLE_API_KEY")},
-	}
-
-	youtubeService, err = youtube.New(client)
+	var scheduler = gocron.NewScheduler(time.Local)
+	_, err = scheduler.Every(6).Hours().StartImmediately().StartAt(time.Now().Add(time.Second * 2)).Do(cacheAutoPlaylistItems)
 	if err != nil {
 		log.Println(err)
 	}
-
-	var scheduler = gocron.NewScheduler(time.Local)
-	scheduler.Every(6).Hours().StartImmediately().StartAt(time.Now().Add(time.Second * 2)).Do(cacheAutoPlaylistItems)
 
 	scheduler.Start()
 }
@@ -79,8 +70,8 @@ func cacheAutoPlaylistItems() {
 		return
 	}
 	sort.Slice(autoPlaylistItemCache, func(i, j int) bool {
-		var titlePartsI = strings.Split(autoPlaylistItemCache[i].Snippet.Title, " - ")
-		var titlePartsJ = strings.Split(autoPlaylistItemCache[j].Snippet.Title, " - ")
+		var titlePartsI = strings.Split(autoPlaylistItemCache[i].Title, " - ")
+		var titlePartsJ = strings.Split(autoPlaylistItemCache[j].Title, " - ")
 
 		return titlePartsI[0] < titlePartsJ[0]
 	})
@@ -88,46 +79,64 @@ func cacheAutoPlaylistItems() {
 	log.Printf("[AP] Cached %d items\n", len(autoPlaylistItemCache))
 }
 
+type Playlist struct {
+	Type    string         `json:"_type"`
+	Entries []PlaylistItem `json:"entries"`
+}
+
+type PlaylistItem struct {
+	Type     string  `json:"_type"`
+	IeKey    string  `json:"ie_key"`
+	Id       string  `json:"id"`
+	Url      string  `json:"url"`
+	Title    string  `json:"title"`
+	Duration float64 `json:"duration"`
+	Uploader string  `json:"uploader"`
+}
+
 // FetchAllPlaylistItems get all the items of a playlist
-func FetchAllPlaylistItems(playlistURL *url.URL) ([]*youtube.PlaylistItem, error) {
-	var listings []*youtube.PlaylistItem
-	var pageToken string
-	for {
-		log.Printf("Fetching from YOUTUBE. We now have %d listings\n", len(listings))
-		youtubeListings, err := youtubeService.
-			PlaylistItems.
-			List("contentDetails,snippet").
-			PlaylistId(playlistURL.Query().Get("list")).
-			MaxResults(50).
-			PageToken(pageToken).
-			Do()
+func FetchAllPlaylistItems(playlistURL *url.URL) ([]PlaylistItem, error) {
+	var playlist Playlist
 
-		if err != nil {
-			return listings, err
-		}
+	var cmdOutput bytes.Buffer
 
-		listings = append(listings, youtubeListings.Items...)
-		pageToken = youtubeListings.NextPageToken
-		if len(pageToken) == 0 {
-			break
-		}
+	var youtubeDL = exec.Command(
+		"youtube-dl",
+		playlistURL.String(),
+		"--dump-single-json",
+		"--skip-download",
+		"--flat-playlist",
+	)
+
+	youtubeDL.Stdout = &cmdOutput
+
+	var err = youtubeDL.Run()
+	if err != nil {
+		return playlist.Entries, err
 	}
 
-	return listings, nil
+	err = json.Unmarshal(cmdOutput.Bytes(), &playlist)
+	if err != nil {
+		return playlist.Entries, err
+	}
+
+	log.Printf("Fetching from YOUTUBE. We now have %d listings\n", len(playlist.Entries))
+
+	return playlist.Entries, nil
 }
 
 // GenerateAutoPlaylistQueueItem get a new item from the auto playlist
-func GenerateAutoPlaylistQueueItem(videoIdsToAvoid []string) (*youtube.PlaylistItem, error) {
-	var chosenListing *youtube.PlaylistItem
+func GenerateAutoPlaylistQueueItem(videoIdsToAvoid []string) (PlaylistItem, error) {
+	var chosenListing PlaylistItem
 
 	if len(autoPlaylistItemCache) == 0 {
-		return nil, fmt.Errorf("Nothing in cache, unable to generate")
+		return chosenListing, fmt.Errorf("Nothing in cache, unable to generate")
 	}
 
 	for {
 		chosenListing = autoPlaylistItemCache[rand.Intn(len(autoPlaylistItemCache))]
 
-		if !stringInSlice(chosenListing.Snippet.ResourceId.VideoId, videoIdsToAvoid) {
+		if !stringInSlice(chosenListing.Id, videoIdsToAvoid) {
 			break
 		}
 
@@ -135,7 +144,7 @@ func GenerateAutoPlaylistQueueItem(videoIdsToAvoid []string) (*youtube.PlaylistI
 		log.Println("Reshuffling, played before")
 	}
 
-	log.Printf("[AP] Chosen video '%s'\n", chosenListing.Snippet.Title)
+	log.Printf("[AP] Chosen video '%s'\n", chosenListing.Title)
 
 	return chosenListing, nil
 }
